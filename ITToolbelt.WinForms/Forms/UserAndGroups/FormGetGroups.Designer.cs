81bb052bd52d4e822856503327d5270954a3980d
Sait ORHAN <saitorhan@outlook.com>
2021-09-29 20:22:32 +0300

    user add group

---
+++ ITToolbelt.WinForms/Forms/UserAndGroups/FormGetGroups.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace ITToolbelt.WinForms.Forms.UserAndGroups
+{
+    partial class FormGetGroups
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGetGroups));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.dataGridViewGroups = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGroups)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonOk);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Image = global::ITToolbelt.WinForms.Properties.Resources.accept;
+            resources.ApplyResources(this.buttonOk, "buttonOk");
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // dataGridViewGroups
+            // 
+            this.dataGridViewGroups.AllowUserToAddRows = false;
+            this.dataGridViewGroups.AllowUserToDeleteRows = false;
+            this.dataGridViewGroups.AutoGenerateColumns = false;
+            this.dataGridViewGroups.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewGroups.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn});
+            this.dataGridViewGroups.DataSource = this.groupBindingSource;
+            resources.ApplyResources(this.dataGridViewGroups, "dataGridViewGroups");
+            this.dataGridViewGroups.Name = "dataGridViewGroups";
+            this.dataGridViewGroups.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            resources.ApplyResources(this.nameDataGridViewTextBoxColumn, "nameDataGridViewTextBoxColumn");
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            resources.ApplyResources(this.descriptionDataGridViewTextBoxColumn, "descriptionDataGridViewTextBoxColumn");
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // groupBindingSource
+            // 
+            this.groupBindingSource.DataSource = typeof(ITToolbelt.Entity.Db.Group);
+            // 
+            // FormGetGroups
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridViewGroups);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormGetGroups";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGroups)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.DataGridView dataGridViewGroups;
+        private System.Windows.Forms.BindingSource groupBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+    }
+}
---
+++ ITToolbelt.WinForms/Forms/UserAndGroups/FormGetGroups.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ITToolbelt.Bll.Managers;
+using ITToolbelt.Entity.Db;
+
+namespace ITToolbelt.WinForms.Forms.UserAndGroups
+{
+    public partial class FormGetGroups : Form
+    {
+        public List<Group> Groups { get; set; }
+        public FormGetGroups()
+        {
+            InitializeComponent();
+            GroupManager groupManager = new GroupManager(GlobalVariables.ConnectInfo);
+            List<Group> groups = groupManager.GetAll();
+            groupBindingSource.DataSource = groups;
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            DataGridViewSelectedRowCollection selectedRows = dataGridViewGroups.SelectedRows;
+            foreach (DataGridViewRow selectedRow in selectedRows)
+            {
+                if (selectedRow.DataBoundItem is Group)
+                {
+                    Groups.Add(selectedRow.DataBoundItem as Group);
+                }
+            }
+
+            Close();
+        }
+    }
+}
