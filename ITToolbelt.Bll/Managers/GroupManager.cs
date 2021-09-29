0d6d12750278b2e384973229cf01d8a7e72d4a75
Sait ORHAN <saitorhan@outlook.com>
2021-09-29 22:29:04 +0300

    group manager

--- ITToolbelt.Bll/Managers/GroupManager.cs
+++ ITToolbelt.Bll/Managers/GroupManager.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation.Results;
 using ITToolbelt.Bll.ExternalLibraries;
+using ITToolbelt.Bll.Validators;
 using ITToolbelt.Dal.Abstract;
 using ITToolbelt.Entity.Db;
 using ITToolbelt.Entity.EntityClass;
@@ -24,6 +28,43 @@ namespace ITToolbelt.Bll.Managers
         public List<Group> GetUserGroups(int userId)
         {
             return iGroupDal.GetUserGroups(userId);
+        }
+
+        public Tuple<bool, List<string>> Add(Group group)
+        {
+            List<string> messages = new List<string>();
+            GroupValidator groupValidator = new GroupValidator(iGroupDal);
+            ValidationResult validationResult = groupValidator.Validate(group);
+            if (!validationResult.IsValid)
+            {
+                IEnumerable<string> enumerable = validationResult.Errors.Select(m => m.ErrorMessage);
+                messages.AddRange(enumerable);
+                return new Tuple<bool, List<string>>(false, messages);
+            }
+
+            Group add = iGroupDal.Add(group);
+            return new Tuple<bool, List<string>>(add != null, null);
+        }
+        public Tuple<bool, List<string>> Update(Group group)
+        {
+            List<string> messages = new List<string>();
+            GroupValidator groupValidator = new GroupValidator(iGroupDal);
+            ValidationResult validationResult = groupValidator.Validate(group);
+            if (!validationResult.IsValid)
+            {
+                IEnumerable<string> enumerable = validationResult.Errors.Select(m => m.ErrorMessage);
+                messages.AddRange(enumerable);
+                return new Tuple<bool, List<string>>(false, messages);
+            }
+
+            Group add = iGroupDal.Update(group);
+            return new Tuple<bool, List<string>>(add != null, null);
+        }
+
+        public Tuple<bool, List<string>> Delete(int groupId)
+        {
+            bool result = iGroupDal.Delete(groupId);
+            return new Tuple<bool, List<string>>(result, null);
         }
     }
 }
---
+++ ITToolbelt.Bll/Validators/GroupValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using ITToolbelt.Dal.Abstract;
+using ITToolbelt.Entity.Db;
+
+namespace ITToolbelt.Bll.Validators
+{
+    public class GroupValidator : AbstractValidator<Group>
+    {
+        private readonly IGroupDal iGroupDal;
+
+        public GroupValidator(IGroupDal iGroupDal)
+        {
+            this.iGroupDal = iGroupDal;
+        }
+    }
+}
