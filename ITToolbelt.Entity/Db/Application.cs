e19b146df74819a17526082e103acc37a297193c
Sait ORHAN <saitorhan@outlook.com>
2021-10-09 22:56:06 +0300

    application db class #5

---
+++ ITToolbelt.Entity/Db/Application.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITToolbelt.Entity.Db
+{
+    public class Application
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Version { get; set; }
+        public string BuiltType { get; set; }
+    }
+}
