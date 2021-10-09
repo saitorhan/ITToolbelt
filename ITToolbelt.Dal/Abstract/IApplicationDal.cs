a6ae569e8f00179d3863ce04f2c95c63d3b0d373
Sait ORHAN <saitorhan@outlook.com>
2021-10-09 23:47:23 +0300

    application interface #5

---
+++ ITToolbelt.Dal/Abstract/IApplicationDal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ITToolbelt.Entity.Db;
+
+namespace ITToolbelt.Dal.Abstract
+{
+    public interface IApplicationDal
+    {
+        List<Application> GelAll();
+        Application Add(Application application);
+        Application Update(Application application);
+        bool Delete(int appId);
+    }
+}
