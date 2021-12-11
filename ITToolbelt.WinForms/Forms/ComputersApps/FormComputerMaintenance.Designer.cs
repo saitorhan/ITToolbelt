620d4307143cd579eed6b660810d9db63ea25b91
Sait ORHAN <saitorhan@kodlagelistir.com>
2021-12-11 20:22:07 +0300

    framework değişti

---
+++ ITToolbelt.WinForms/Forms/ComputersApps/FormComputerMaintenance.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace ITToolbelt.WinForms.Forms.ComputersApps
+{
+    partial class FormComputerMaintenance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormApplications));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonResfresh = new System.Windows.Forms.Button();
+            this.buttonColumnSelection = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewApplications = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.publisherDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.versionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.builtTypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.applicationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBarStatus = new System.Windows.Forms.ToolStripProgressBar();
+            this.backgroundWorkerWorker = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewApplications)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.applicationBindingSource)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonResfresh);
+            this.panel1.Controls.Add(this.buttonColumnSelection);
+            this.panel1.Controls.Add(this.buttonRemove);
+            this.panel1.Controls.Add(this.buttonUpdate);
+            this.panel1.Controls.Add(this.buttonAdd);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // buttonResfresh
+            // 
+            resources.ApplyResources(this.buttonResfresh, "buttonResfresh");
+            this.buttonResfresh.Image = global::ITToolbelt.WinForms.Properties.Resources.refresh;
+            this.buttonResfresh.Name = "buttonResfresh";
+            this.buttonResfresh.UseVisualStyleBackColor = true;
+            this.buttonResfresh.Click += new System.EventHandler(this.buttonResfresh_Click);
+            // 
+            // buttonColumnSelection
+            // 
+            resources.ApplyResources(this.buttonColumnSelection, "buttonColumnSelection");
+            this.buttonColumnSelection.Image = global::ITToolbelt.WinForms.Properties.Resources.edit_page;
+            this.buttonColumnSelection.Name = "buttonColumnSelection";
+            this.buttonColumnSelection.UseVisualStyleBackColor = true;
+            this.buttonColumnSelection.Click += new System.EventHandler(this.buttonColumnSelection_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Image = global::ITToolbelt.WinForms.Properties.Resources.delete;
+            resources.ApplyResources(this.buttonRemove, "buttonRemove");
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Image = global::ITToolbelt.WinForms.Properties.Resources.edit_page;
+            resources.ApplyResources(this.buttonUpdate, "buttonUpdate");
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Image = global::ITToolbelt.WinForms.Properties.Resources.add;
+            resources.ApplyResources(this.buttonAdd, "buttonAdd");
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridViewApplications);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // dataGridViewApplications
+            // 
+            this.dataGridViewApplications.AllowUserToAddRows = false;
+            this.dataGridViewApplications.AllowUserToDeleteRows = false;
+            this.dataGridViewApplications.AllowUserToOrderColumns = true;
+            this.dataGridViewApplications.AutoGenerateColumns = false;
+            this.dataGridViewApplications.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewApplications.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.publisherDataGridViewTextBoxColumn,
+            this.versionDataGridViewTextBoxColumn,
+            this.builtTypeDataGridViewTextBoxColumn});
+            this.dataGridViewApplications.DataSource = this.applicationBindingSource;
+            resources.ApplyResources(this.dataGridViewApplications, "dataGridViewApplications");
+            this.dataGridViewApplications.Name = "dataGridViewApplications";
+            this.dataGridViewApplications.ReadOnly = true;
+            this.dataGridViewApplications.Tag = "9F2C783FB9F947EE834A9ABBB56B5373";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            resources.ApplyResources(this.nameDataGridViewTextBoxColumn, "nameDataGridViewTextBoxColumn");
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // publisherDataGridViewTextBoxColumn
+            // 
+            this.publisherDataGridViewTextBoxColumn.DataPropertyName = "Publisher";
+            resources.ApplyResources(this.publisherDataGridViewTextBoxColumn, "publisherDataGridViewTextBoxColumn");
+            this.publisherDataGridViewTextBoxColumn.Name = "publisherDataGridViewTextBoxColumn";
+            this.publisherDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // versionDataGridViewTextBoxColumn
+            // 
+            this.versionDataGridViewTextBoxColumn.DataPropertyName = "Version";
+            resources.ApplyResources(this.versionDataGridViewTextBoxColumn, "versionDataGridViewTextBoxColumn");
+            this.versionDataGridViewTextBoxColumn.Name = "versionDataGridViewTextBoxColumn";
+            this.versionDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // builtTypeDataGridViewTextBoxColumn
+            // 
+            this.builtTypeDataGridViewTextBoxColumn.DataPropertyName = "BuiltType";
+            resources.ApplyResources(this.builtTypeDataGridViewTextBoxColumn, "builtTypeDataGridViewTextBoxColumn");
+            this.builtTypeDataGridViewTextBoxColumn.Name = "builtTypeDataGridViewTextBoxColumn";
+            this.builtTypeDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // applicationBindingSource
+            // 
+            this.applicationBindingSource.DataSource = typeof(ITToolbelt.Entity.Db.Application);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBarStatus});
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            // 
+            // toolStripProgressBarStatus
+            // 
+            this.toolStripProgressBarStatus.Name = "toolStripProgressBarStatus";
+            resources.ApplyResources(this.toolStripProgressBarStatus, "toolStripProgressBarStatus");
+            // 
+            // backgroundWorkerWorker
+            // 
+            this.backgroundWorkerWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerWorker_DoWork);
+            this.backgroundWorkerWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerWorker_RunWorkerCompleted);
+            // 
+            // FormApplications
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormApplications";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormUsers_FormClosing);
+            this.Load += new System.EventHandler(this.FormUsers_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewApplications)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.applicationBindingSource)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridViewApplications;
+        private System.Windows.Forms.BindingSource applicationBindingSource;
+        private System.Windows.Forms.Button buttonResfresh;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBarStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerWorker;
+        private System.Windows.Forms.Button buttonColumnSelection;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn publisherDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn versionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn builtTypeDataGridViewTextBoxColumn;
+    }
+}
---
+++ ITToolbelt.WinForms/Forms/ComputersApps/FormComputerMaintenance.cs
@@ -0,0 +1,150 @@
+﻿using ITToolbelt.WinForms.ExtensionMethods;
+using ITToolbelt.WinForms.Forms.ControlSpesifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Application = ITToolbelt.Entity.Db.Application;
+
+namespace ITToolbelt.WinForms.Forms.ComputersApps
+{
+    public partial class FormComputerMaintenance : Form
+    {
+        private WorkerStatus wStatus;
+        public FormComputerMaintenance()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorkerWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            switch (wStatus)
+            {
+                case WorkerStatus.RefreshData:
+                    RefreshData();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void backgroundWorkerWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            toolStripProgressBarStatus.StartStopMarque();
+        }
+
+        private void RefreshData()
+        {
+            //ApplicationManeger applicationManeger = new ApplicationManeger(GlobalVariables.ConnectInfo);
+            //List<Application> applications = applicationManeger.GetAll();
+
+            //if (dataGridViewApplications.InvokeRequired)
+            //{
+            //    dataGridViewApplications.Invoke(new Action(delegate
+            //    {
+            //        applicationBindingSource.DataSource = applications;
+            //    }));
+            //}
+            //else
+            //{
+            //    applicationBindingSource.DataSource = applications;
+            //}
+        }
+
+        enum WorkerStatus
+        {
+            RefreshData
+        }
+
+        private void FormUsers_Load(object sender, EventArgs e)
+        {
+            dataGridViewApplications.LoadGridColumnStatus();
+
+            wStatus = WorkerStatus.RefreshData;
+            toolStripProgressBarStatus.StartStopMarque();
+            backgroundWorkerWorker.RunWorkerAsync();
+        }
+
+        private void buttonResfresh_Click(object sender, EventArgs e)
+        {
+            wStatus = WorkerStatus.RefreshData;
+            toolStripProgressBarStatus.StartStopMarque();
+            backgroundWorkerWorker.RunWorkerAsync();
+        }
+
+        private void FormUsers_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            dataGridViewApplications.SaveGridColumnStatus();
+        }
+
+        private void buttonColumnSelection_Click(object sender, EventArgs e)
+        {
+            FormGridColumnSelection formGridColumn = new FormGridColumnSelection(dataGridViewApplications.Columns);
+            formGridColumn.ShowDialog();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            FormApplication formUser = new FormApplication();
+            formUser.ShowDialog();
+
+            wStatus = WorkerStatus.RefreshData;
+            toolStripProgressBarStatus.StartStopMarque();
+            backgroundWorkerWorker.RunWorkerAsync();
+
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewApplications.SelectedRows.Count == 0 || !(dataGridViewApplications.SelectedRows[0].DataBoundItem is Application))
+            {
+                return;
+            }
+
+            Application application = dataGridViewApplications.SelectedRows[0].DataBoundItem as Application;
+            if (application == null)
+            {
+                return;
+            }
+
+            //FormApplication formUser = new FormApplication(application);
+            //formUser.ShowDialog();
+
+            //wStatus = WorkerStatus.RefreshData;
+            //toolStripProgressBarStatus.StartStopMarque();
+            //backgroundWorkerWorker.RunWorkerAsync();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewApplications.SelectedRows.Count == 0 || !(dataGridViewApplications.SelectedRows[0].DataBoundItem is Application))
+            {
+                return;
+            }
+
+            Application application = dataGridViewApplications.SelectedRows[0].DataBoundItem as Application;
+            if (application == null)
+            {
+                return;
+            }
+            if (GlobalMethods.DeleteConfirm(application.Name) != DialogResult.Yes)
+            {
+                return;
+            }
+            //ApplicationManeger applicationManeger = new ApplicationManeger(GlobalVariables.ConnectInfo);
+            //Tuple<bool, List<string>> delete = applicationManeger.Delete(application.Id);
+            //delete.ShowDialog();
+            //if (delete.Item1)
+            //{
+            //    wStatus = WorkerStatus.RefreshData;
+            //    toolStripProgressBarStatus.StartStopMarque();
+            //    backgroundWorkerWorker.RunWorkerAsync();
+            //}
+        }
+    }
+}
